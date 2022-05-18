1c24c965601d1162fb3ad3d23eca1548bd3eb69f
Shubham Kumar <shubham35godda@gmail.com>
2022-05-18 23:35:19 +0530

    created form

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className='main'><div className='container'>
+      <form>
+      <label for="fname">First Name</label>
+  <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+  <label for="lname">Last Name</label>
+  <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+  <label for="lname">PassWord</label>
+  <input type="password" id="lname" name="lastname" placeholder="Enter your password.."/>
+  <label for="lname">MobileNo</label>
+  <input type="number" id="lname" name="lastname" placeholder="Enter your mobile no..."/>
+  <div style={{margin:"auto", width:"100px"}}><button>Submit</button></div>
+
+  
+
+
+      </form>
+    </div></div>
+    )
+  }
 }
-
-export default App;
