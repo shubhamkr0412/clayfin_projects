7277c8db69b60aea007c09b6751cd1cedea796f9
Shubham Kumar <shubham35godda@gmail.com>
2022-05-19 23:48:30 +0530

    updated

--- forms/src/App.js
+++ forms/src/App.js
@@ -2,76 +2,106 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       name: "",
-      lname:"",
-      email:"",
-      password:"",
-      num:"",
-nameE:false,
+      nameStatus:false,
+      lname: "",
+      email: "",
+      emailStatus:"",
+      password: "",
+      passwordStatus:false,
+      num: "",
+      numStatus:false,
+      nameE: "",
+      lnamE: "",
+      emailE: "",
+      numE: "",
+      passwordE: "",
     };
-
   }
- 
- 
-  handleLname = (e) => {
-    
-    this.setState({ lname: e.target.value  })
-    console.log(this.state.lname);
-  };
-  handlePass = (e) => {
-    
-    this.setState({ password: e.target.value  })
-    console.log(this.state.password);
-  };
-  handleNo = (e) => {
-  
-    this.setState({ num: e.target.value  })
-    console.log(this.state.num);
-  };
+
   render() {
     this.handleName = (e) => {
- 
-      this.setState({ name: e.target.value  })
+      this.setState({ name: e.target.value });
       let regex = /^[A-Z]([0-9a-zA-Z]){2,10}$/;
-      if(regex.test(this.state.name )  ){
-        this.setState({ nameE: " "  })
-       
-      }
-     
-      else {
-        this.setState({ nameE: "Invalid Name "  })
+      if (regex.test(this.state.name)) {
+        this.setState({ nameE: " " });
+        this.setState({ nameStatus: true });
+      } else {
+        this.setState({ nameE: "invalid " });
       }
     };
-    
+    this.handleLname = (e) => {
+      this.setState({ lname: e.target.value });
+      let regexx = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+      if (regexx.test(this.state.lname)) {
+        this.setState({ lnamE: " " });
+        this.setState({ emailStatus: true });
+      } else {
+        this.setState({ lnamE: "invalid " });
+      }
+    };
+    this.handleNo = (e) => {
+      this.setState({ num: e.target.value });
+      let regex = /^([0-9]){9}$/;
+      if (regex.test(this.state.num)) {
+        this.setState({ numE: " " });
+        this.setState({ numStatus: true });
+      } else {
+        this.setState({ numE: "invalid " });
+      }
+    };
+    this.handlePass = (e) => {
+      this.setState({ password: e.target.value });
+      let regex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+      if (regex.test(this.state.password)) {
+        this.setState({ passwordE: " " });
+        this.setState({ passwordStatus: true });
+      } else {
+        this.setState({ passwordE: "invalid " });
+      }
+    };
+    this.handleSubmit=(e)=>{
+      e.preventDefault();
+      
+      if(this.state.nameStatus && this.state.emailStatus && this.state.numStatus && this.state.passwordStatus){
+alert("Form Submitted Succesfully")
+      }
+      else{
+        alert("try filling the form correctly");
+       
+      }
+    }
+
     return (
       <div className="main">
         <div className="container">
           <form>
             <label for="fname">First Name</label>
             <input
-             onChange={this.handleName}
+              onChange={this.handleName}
               className="inp"
-            
-              
               name="firstname"
               placeholder="Your name.."
-            />  <label for="lname">{this.state.nameE}</label>
-            
-        
-
-          
-            
-            <label for="lname">Last Name</label>
+            />{" "}
+            <div>
+              {" "}
+              <label for="lname">{this.state.nameE}</label>
+            </div>
+            <label for="lname">Email</label>
             <input
-              onBlur={this.handleLname}
+              onChange={this.handleLname}
               className="inp"
               id="lname"
               name="lastname"
-              placeholder="Your last name.."
+              placeholder="Your email.."
             />
+            <div>
+              <label for="lname">{this.state.lnamE}</label>
+            </div>
             <label for="lname">PassWord</label>
             <input
               onChange={this.handlePass}
@@ -80,6 +110,9 @@ nameE:false,
               name="lastname"
               placeholder="Enter your password.."
             />
+            <div>
+              <label for="lname">{this.state.passwordE}</label>
+            </div>
             <label for="lname">MobileNo</label>
             <input
               onChange={this.handleNo}
@@ -88,8 +121,11 @@ nameE:false,
               name="lastname"
               placeholder="Enter your mobile no..."
             />
+            <div>
+              <label for="lname">{this.state.numE}</label>
+            </div>
             <div style={{ margin: "auto", width: "100px" }}>
-              <button>Submit</button>
+              <button onClick={this.handleSubmit}>Submit</button>
             </div>
           </form>
         </div>
