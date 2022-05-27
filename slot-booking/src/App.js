6e618930d14ceee639a8b6039cdfe982b7df3341
Shubham Kumar <shubham35godda@gmail.com>
2022-05-27 11:39:22 +0530

    updated

--- slot-booking/src/App.js
+++ slot-booking/src/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import "./App.css";
-import { GoogleLogin } from "react-google-login";
+
 const App = () => {
-const responseGoogle=(response)=>{
-  console.log(response);
-}
-const responseError=(error)=>{
-  console.log(error);
-}
+
 
   return (
     <div>
@@ -15,18 +10,7 @@ const responseError=(error)=>{
         {" "}
         <h1>Google Calender Api</h1>
       </div>
-      <div>
-        <GoogleLogin
-          clientId="112204028961-ng9f0jub48d9tsiktsm8faopsrrsf550.apps.googleusercontent.com"
-          buttonText="Sign in & Authorize Calender"
-          onSuccess={responseGoogle}
-          onFailure={responseError}
-          cookiePolicy={"single_host_origin"}
-          responseType="code"
-          accessType="offline"
-          scope="openid email profile https://www.googleapis.com/auth/calendar "
-        />
-      </div>
+     
     </div>
   );
 };
--- slot-booking/src/config.js
+++ slot-booking/src/config.js
@@ -1,39 +1,7 @@
-export  const GOOGLE_API_KEY='AAIzaSyDiX7shoib427UUN3KTzWp7m4sZ982_2uU';
-
-// export const 
-//   GOOGLE_API_KEY = 'AAIzaSyDiX7shoib427UUN3KTzWp7m4sZ982_2uU',
-//   CALENDAR_ID = 'sqm2udi1adf6ds7gj9h81tuet8@group.calendar.google.com';
 
 
-//   ClientId,
-//   112204028961-ng9f0jub48d9tsiktsm8faopsrrsf550.apps.googleusercontent.com
-
-//   SecretKey
-//   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
+ export const 
+  GOOGLE_API_KEY = 'AIzaSyBkYqU_Y7uPMMlX3csUxTyFVzutr0vM6zs',
+  CALENDAR_ID = 'sqm2udi1adf6ds7gj9h81tuet8@group.calendar.google.com';
 
 
-// Api Key
-// AIzaSyDPGWso-jPNhzrITROrFdKnmI6q02sxE_E
-
-// <div className="App">
-// {" "}
-// <h1>Google Calender Api</h1>
-// </div>
-// <div>
-// <GoogleLogin
-//   clientId="112204028961-ng9f0jub48d9tsiktsm8faopsrrsf550.apps.googleusercontent.com"
-//   buttonText="Sign in & Authorize Calender"
-//   onSuccess={responseGoogle}
-//   onFailure={responseError}
-//   cookiePolicy={"single_host_origin"}
-//   responseType="code"
-//   accessType="offline"
-//   scope="openid email profile https://www.googleapis.com/auth/calendar "
-// />
-// </div>
-// const responseGoogle=(response)=>{
-//   console.log(response);
-// }
-// const responseError=(error)=>{
-//   console.log(error);
-// }
